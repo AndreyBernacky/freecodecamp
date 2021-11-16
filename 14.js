dfeb111d3008354886e50697a9677fb284ccd5a5
AndreyBernacky <dronskmne@gmail.com>
2021-11-16 10:00:11 +0300

    FreeCodeCamp tasks 21 - 24

---
+++ 14.js
@@ -0,0 +1,39 @@
+// task 21
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+// task 22
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+// task 23
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+// task 24 
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
