1a850c9bb30b2ba7f7499fc4fe4ead3460f055b7
AndreyBernacky <dronskmne@gmail.com>
2021-11-17 11:01:28 +0300

    FreeCodeCamp tasks 39 - 42

---
+++ 19.js
@@ -0,0 +1,18 @@
+// Task 39
+
+const myArray = ["string", 123];
+
+// Task 40
+
+const myArray = [["string", 123], ["Name", 321]];
+
+// Task 41
+
+const myArray = [50, 60, 70];
+const myData = myArray[0];
+
+// Task 42
+
+const myArray = [18, 64, 99];
+
+myArray[0] = 45;
