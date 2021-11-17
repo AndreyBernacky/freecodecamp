bae42d625afec0a81173bc59411688b3b62f097b
AndreyBernacky <dronskmne@gmail.com>
2021-11-17 11:14:36 +0300

    FreeCodeCamp tasks 43 - 47

---
+++ 20.js
@@ -0,0 +1,35 @@
+// Task 43
+
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
+
+  // Task 44
+
+const myArray = [["John", 23], ["cat", 2]];
+
+myArray.push(["dog", 3]);
+
+  // Task 45
+
+const myArray = [["John", 23], ["cat", 2]];
+
+const removedFromMyArray = myArray.pop();
+
+  // Task 46
+const myArray = [["John", 23], ["dog", 3]];
+
+const removedFromMyArray = myArray.shift();
+
+  // Task 47
+
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+myArray.unshift(["Paul", 35]);
+
