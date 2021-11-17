11201dd5650b7b512e3bdc9f1c71710a65626cd7
AndreyBernacky <dronskmne@gmail.com>
2021-11-17 10:14:37 +0300

    FreeCodeCamp tasks 28 - 31

---
+++ 16.js
@@ -0,0 +1,19 @@
+//Task 28
+
+const myStr = "This is the start. " + "This is the end.";
+
+//Task 29
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+//Task 30
+
+const myName = "Andrey";
+const myStr = "My name is " + myName + " and I am well!";
+
+//Task 31
+
+const someAdjective = "Cool!";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
