a15d255da9b81ea2bd5ba58b7350632f8cc4256b
AndreyBernacky <dronskmne@gmail.com>
2021-11-17 10:41:40 +0300

    FreeCodeCamp task 38

---
+++ 18.js
@@ -0,0 +1,8 @@
+// Task 38
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "I have a " + myNoun + ". It is " + myAdjective + " It " + myVerb + " very " + myAdverb; 
