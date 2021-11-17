8d77117223dd769d95bae3361378c9b602a24d41
AndreyBernacky <dronskmne@gmail.com>
2021-11-17 09:58:53 +0300

    FreeCodeCamp tasks 25 - 27

---
+++ 15.js
@@ -0,0 +1,11 @@
+//Task 25
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+
+//Task 26
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//Task 27
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
