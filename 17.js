89e70a7cab8a5f267533f5a65930e4e45d59269b
AndreyBernacky <dronskmne@gmail.com>
2021-11-17 10:29:49 +0300

    FreeCodeCamp tasks 32 - 37

---
+++ 17.js
@@ -0,0 +1,37 @@
+// Task 32
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+
+// Task 33
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0]; 
+
+// Task 34
+
+let myStr = "Jello World";
+
+myStr = "Hello World"; 
+
+// Task 35
+
+const lastName = "Lovelace";
+
+const thirdLetterOfLastName = lastName[2]; 
+
+// Task 36
+
+const lastName = "Lovelace";
+
+const lastLetterOfLastName = lastName[lastName.length -1]; 
+
+// Task 37 
+
+const lastName = "Lovelace";
+
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; 
