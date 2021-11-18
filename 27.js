13671c30a175dde809836925b5fd372782c1c7a0
AndreyBernacky <dronskmne@gmail.com>
2021-11-18 09:12:52 +0300

    FreeCodeCamp task 54

---
+++ 27.js
@@ -0,0 +1,12 @@
+// Task 54
+
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  const outerWear = "sweater";
+  return outerWear;
+}
+
+console.log(outerWear);
+myOutfit();
+console.log(outerWear);
