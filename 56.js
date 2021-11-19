be24b721547cf994414b0abe69ab5eec1e9e4568
AndreyBernacky <dronskmne@gmail.com>
2021-11-19 10:03:31 +0300

    FreeCodeCamp tasks 83 - 89

---
+++ 56.js
@@ -0,0 +1,79 @@
+// Task 83
+
+const myDog = {
+
+    "name": "Rex",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["people", "dog"]
+  
+  };
+
+// Task 84
+
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  const hatValue = testObj.hat;      
+  const shirtValue = testObj.shirt;   
+
+// Task 85
+
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  const entreeValue = testObj["an entree"];   
+  const drinkValue = testObj["the drink"];    
+
+// Task 86
+
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  const playerNumber = 16;  
+  const player = testObj[playerNumber];  
+
+// Task 87
+
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.name = "Happy Coder";
+
+  // Task 88
+
+  const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.bark = "woof";
+
+    // Task 89
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  delete myDog.tails;
+
+  
