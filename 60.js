ae513ad9efa82a4296932fb42a2fde276609e3a9
AndreyBernacky <dronskmne@gmail.com>
2021-11-19 12:45:26 +0300

    FreeCodeCamp tasks 93 and 94

---
+++ 60.js
@@ -0,0 +1,38 @@
+// Task 93
+
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
+
+// Task 94
+
+const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  const secondTree = myPlants[1].list[1];
