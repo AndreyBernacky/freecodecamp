7592b2a761a72803fe83301452bfb3178964d296
AndreyBernacky <dronskmne@gmail.com>
2021-11-20 17:28:28 +0300

    FreeCodeCamp tasks 96 - 99

---
+++ 62.js
@@ -0,0 +1,33 @@
+// Task 96
+
+const myArray = [];
+let i = 5;
+
+while (i >= 0){
+  myArray.push(i);
+  i--;
+}
+
+// Task 97
+
+const myArray = [];
+
+for(let i = 1; i < 6; i++){
+  myArray.push(i);
+}
+
+// Task 98
+
+const myArray = [];
+
+for (let i = 1; i < 10; i += 2){
+  myArray.push(i);
+}
+
+// Task 99
+
+const myArray = [];
+
+for(let i = 9; i > 0; i -= 2){
+  myArray.push(i);
+}
